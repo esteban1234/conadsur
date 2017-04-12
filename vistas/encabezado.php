facee7d03dc141c4f44f8af7dbcef752afc2ae32
esteban1234 <tva_libra@hotmail.com>
2017-04-12 09:09:40 -0500

    metas

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -6,10 +6,10 @@
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<title>CONADSUR</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
-  	<META NAME="Title" content="MATENDA PROMOTORA en ciudad de mérida.">
+  	<META NAME="Title" content="CONADSUR S.C.P en ciudad de méxico.">
   	<META NAME="Author" CONTENT="[]"/>
-  	<META name="Description" content="Somos una compañía ubicada en la ciudad de mérida, yucatan, dedicados a ofrecer servicios de venta de inmobiliarios, Servicios de renta de inmobiliarios."/>
-  	<META NAME="Keywords" CONTENT="venta de inmobiliarios en mérida, renta de inmobiliarios en mérida, renta de inmobiliarios, renta de inmobiliarios en mérida yucatan, venta de inmobiliarios en merida yucatan."/>
+  	<META name="Description" content="Somos una compañía ubicada en la ciudad de mexico, dedicados a ofrecer servicios de Transporte turístico por tierra, Servicios de excursiones y paquetes turísticos para agencias de viajes."/>
+  	<META NAME="Keywords" CONTENT="Transporte turístico por tierra en mexico, excursiones y paquetes turísticos para agencias de viajes en mexico, excursiones y paquetes turísticos para agencias de viajes, Transporte turístico por tierra en merida."/>
   	<link rel="stylesheet" href="css/bootstrap.css">
   	<link rel="stylesheet" href="css/iconos.min.css">
 	<link rel="stylesheet" href="css/estilos.css">
